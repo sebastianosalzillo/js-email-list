b7fb9f7a08177ebb8922977b34eee70cc702fadb
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-11-18 15:49:46 +0100

    Feat: completato

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-    console.log(resp);
-});
+const showMail = document.getElementById("show-email");
+
+
+async function randomMail() {
+    const response = await axios.get("https://flynn.boolean.careers/exercises/api/random/mail");
+    return response.data.response; 
+}
+
+
+async function generateMails(count) {
+    const emails = [];
+    for (let i = 0; i < count; i++) {
+        const email = await randomMail(); 
+        if (email) {
+            emails.push(email); 
+        }
+    }
+    console.log("Tutte le email:", emails);
+
+    
+    showMail.innerHTML = `<ul>${emails.map(email => `<li>${email}</li>`).join("")}</ul>`;
+
+}
+
+
+generateMails(10);
